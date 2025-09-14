281b78e84aa740c21eec41d964246eb06c364865
Wimage03 <nafi314159@gmail.com>
2025-09-13 22:18:52 -0400

    render 3D island

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,49 @@
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import Loader from '../Components/Loader.jsx';
+import Island from '../models/Island.jsx';
+
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
+    POPUP
+</div> */}
+const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0.1, -6.5, -43];
+    let screenRotation = [0, 4.7, 0];
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, screenRotation];
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far:1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  )
+}
+
+export default Home
---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>Project section under construction</div>
+  )
+}
+
+export default Projects
